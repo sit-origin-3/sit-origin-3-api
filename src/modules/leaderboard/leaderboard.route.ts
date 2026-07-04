3180da358b26a6554f1025c576a4428806ad847b
bunthree <224548114+bunthree@users.noreply.github.com>
2026-07-05 03:40:44 +0700

    change leaderboard need to login to see

--- src/modules/leaderboard/leaderboard.route.ts
+++ src/modules/leaderboard/leaderboard.route.ts
@@ -1,7 +1,8 @@
 import type { FastifyInstance } from "fastify"
+import * as hook from "../../hooks/auth.hook.js"
 import * as controller from "./leaderboard.controller.js"
 
 export async function leaderboardRoutes(app: FastifyInstance) {
   // GET /api/leaderboard/stream
-  app.get("/stream", controller.streamLeaderboard)
+  app.get("/stream", { preHandler: [hook.requireAuth] }, controller.streamLeaderboard)
 }
