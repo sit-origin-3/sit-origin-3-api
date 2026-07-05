2055ea6b31371ff264ca0237f1c95bea08583dcb
bunthree <224548114+bunthree@users.noreply.github.com>
2026-07-05 15:23:05 +0700

    fix leaderboard

--- src/modules/leaderboard/leaderboard.controller.ts
+++ src/modules/leaderboard/leaderboard.controller.ts
@@ -48,10 +48,11 @@ export async function streamLeaderboard(
   req: FastifyRequest,
   reply: FastifyReply,
 ) {
-  reply.raw.setHeader("Content-Type", "text/event-stream")
-  reply.raw.setHeader("Cache-Control", "no-cache")
-  reply.raw.setHeader("Connection", "keep-alive")
-  reply.raw.flushHeaders()
+  reply.header("Content-Type", "text/event-stream")
+  reply.header("Cache-Control", "no-cache")
+  reply.header("Connection", "keep-alive")
+  await reply.send("")
+
 
   const send = async () => {
     const users = await getLeaderboardData()
