94584602a80ff5c6ba18766936fc03c3dde4d706
bunthree <224548114+bunthree@users.noreply.github.com>
2026-07-05 15:19:08 +0700

    fix bug on cors.ts

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -3,7 +3,8 @@ import cors from "@fastify/cors"
 
 export const corsPlugin = fp(async (app) => {
   await app.register(cors, {
-    origin: true,
+    origin: ["http://localhost:5173"],
     credentials: true,
+    exposedHeaders: ["Content-Type", "Cache-Control"],
   })
 })
