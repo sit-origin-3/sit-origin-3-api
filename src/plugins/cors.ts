14db2288cd9e04ddcd8d13be41e1d5f5bb0c0ded
bunthree <224548114+bunthree@users.noreply.github.com>
2026-07-06 22:00:18 +0700

    fix:cors.ts

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -3,7 +3,7 @@ import cors from "@fastify/cors"
 
 export const corsPlugin = fp(async (app) => {
   await app.register(cors, {
-    origin: ["http://localhost:5173", "SIT-Origin.sit.kmutt.ac.th"],
+    origin: ["http://localhost:5173", "https://SIT-Origin.sit.kmutt.ac.th"],
     credentials: true,
     exposedHeaders: ["Content-Type", "Cache-Control"],
   })
